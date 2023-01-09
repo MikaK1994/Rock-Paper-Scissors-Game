245c288a698c7ca8c37df07bf5a1af74e30f9eb7
Mika Koivisto <mika.koivisto@hotmail.com>
2023-01-09 17:54:36 +0200

    First commit

---
+++ RPS-Game.py
@@ -0,0 +1,67 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit the game.").lower()
+    if user_input == "q":
+        break
+    
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    if user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    if user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    if user_input == "rock" and computer_pick == "paper":
+        print("You lost!")
+        computer_wins += 1
+        continue
+
+    if user_input == "paper" and computer_pick == "scissors":
+        print("You lost!")
+        computer_wins += 1
+        continue
+
+    if user_input == "scissors" and computer_pick == "rock":
+        print("You lost!")
+        computer_wins += 1
+        continue
+
+    if user_input == "rock" and computer_pick == "rock":
+        print("It's a tie!")
+        continue
+
+    if user_input == "paper" and computer_pick == "paper":
+        print("It's a tie!")
+        continue
+
+    if user_input == "scissors" and computer_pick == "scissors":
+        print("It's a tie!")
+        continue
+
+print("You won", user_wins, "times.")
+print("Computer won", computer_wins, "times.")
+print("Goodbye!")
+
+
